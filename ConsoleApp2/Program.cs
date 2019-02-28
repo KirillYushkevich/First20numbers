64633b43ea259d0d0daa7d8429a58b765c83cc1d
KirillYushkevich <47444317+KirillYushkevich@users.noreply.github.com>
2019-02-28 17:06:32 +0300

    Solution

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fibonacciNumbers = new List<int> { 1, 1 };
+            for(int i=2;i<20;i++)
+            {
+                fibonacciNumbers.Add(fibonacciNumbers[i-1]+fibonacciNumbers[i-2]);
+            }
+            foreach (var item in fibonacciNumbers)
+                Console.WriteLine(item);
+        }
+
+    }
+}
